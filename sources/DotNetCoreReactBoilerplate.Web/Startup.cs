23ddf6059a2eae09689215114467aa4ac21302af
ycadieux <ycadieux@telequebec.tv>
2018-01-29 17:51:17 -0500

    add mvc

--- sources/DotNetCoreReactBoilerplate.Web/Startup.cs
+++ sources/DotNetCoreReactBoilerplate.Web/Startup.cs
@@ -15,19 +15,19 @@ namespace DotNetCoreReactBoilerplate.Web
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseStaticFiles();
 
-            app.Run(async (context) =>
+            app.UseMvc(routes =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
